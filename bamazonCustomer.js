3514a5a26bde75f79f1194b31b2e8a1a1b237d17
Santiago Enciso <whereispolaris@gmail.com>
2019-08-12 20:52:25 -0500

    Connected application to database and made successful connection.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,25 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+// Create MySQL Connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "BdEBK4Z4U@J7N6;uo9;x",
+    database: "bamazon"
+});
+
+// Connect to MySQL
+connection.connect(function (err) {
+    console.log("Connected as id: " + connection.threadId);
+    startShopping();
+});
+
+function startShopping() {
+    // Display All iterms
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.table(res);
+    });
+}
+
