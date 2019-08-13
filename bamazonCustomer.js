66c3d4ff52bb5ca0affe20cc3b615043418a5f9a
Santiago Enciso <whereispolaris@gmail.com>
2019-08-12 21:47:12 -0500

    Added inquirer questions to ask user what item they want to buy along with the quantity.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,16 +10,49 @@ const connection = mysql.createConnection({
     database: "bamazon"
 });
 
-// Connect to MySQL
-connection.connect(function (err) {
-    console.log("Connected as id: " + connection.threadId);
-    startShopping();
+// Display All items
+connection.query("SELECT * FROM products", function (err, res) {
+    console.table(res);
 });
 
 function startShopping() {
-    // Display All iterms
-    connection.query("SELECT * FROM products", function (err, res) {
-        console.table(res);
-    });
+
+    inquirer.prompt([
+        {
+            // The first should ask them the ID of the product they would like to buy.
+            type: "input",
+            message: "Please enter the item_id of the product you would like to buy.",
+            name: "item_id",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            // The second message should ask how many units of the product they would like to buy.
+            type: "input",
+            message: "How many would you like to buy? (Enter a number)",
+            name: "quantity",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    ]).then(function (answer) {
+        console.log("You have chosen to buy " + answer.quantity + " Items with the item ID " + answer.item_id);
+    })
 }
+
+// Call startShopping function after half a second. 
+setTimeout(startShopping, 500);
+
+
 
