ba89d0e53180f9c56a4347240ea7c42c2654de58
Santiago Enciso <whereispolaris@gmail.com>
2019-08-14 17:43:34 -0500

    Created function that prompts manager to view/add products or check for low inventory.

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,3 +10,18 @@ const connection = mysql.createConnection({
     password: "BdEBK4Z4U@J7N6;uo9;x",
     database: "bamazon"
 });
+
+function managerStart() {
+    inquirer.prompt({
+        type: "list",
+        message: "Welcome, Mr. Manager. What would you like to do today?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        name: "managerToDo"
+    }).then(function (answer) {
+
+        console.log(answer.managerToDo);
+
+    });
+}
+
+managerStart();
